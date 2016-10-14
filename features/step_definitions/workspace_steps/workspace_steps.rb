e0dd758b00c75180f14c44f7e39622a294cab980
Angelica <Angelica.Rodriguez@fundacion-jala.org>
2016-10-14 16:20:22 -0400

    functional test workspaces

--- features/step_definitions/workspace_steps/workspace_steps.rb
+++ features/step_definitions/workspace_steps/workspace_steps.rb
@@ -18,7 +18,7 @@ end
 When(/^I send a (PUT) request to WorkspacesPut endpoint add the (.*) before created$/) do |method, attribute|
   require_relative '../../../src/requests/Workspaces/workspaces_put'
   json = "{ \"#{attribute}\" : [#{@project_name.id}]}"
-  @http_response = WorkspacesPut.call_and_return_response(@client, method, json, @workspace.id)
+  @http_response, @workspace = WorkspacesPut.call_and_return_response(@client, method, json, @workspace.id)
 end
 
 When(/^I send a (DELETE) request to WorkspacesDelete endpoint$/) do |method|
@@ -40,9 +40,48 @@ Then(/^I expect Status code (\d+) for the PUT request to WorkspacePUT$/) do |htt
   ProjectsDelete.call_and_return_response(@client, 'DELETE', @project_name.id)
 end
 
+Then(/^I expect that response has the attribute name equal to (.*)$/) do |name|
+  require_relative '../../../src/requests/Workspaces/workspaces_get'
+  expect(@workspace.name.to_s).to eql(name.to_s)
+end
 
+Then(/^I expect that the response attribute id be an integer$/) do
+  require_relative '../../../src/requests/Workspaces/workspaces_get'
+  expect(@workspace.id.kind_of?Integer)
+end
 
+Then(/^I expect that the response attribute name be an string$/) do
+  require_relative '../../../src/requests/Workspaces/workspaces_get'
+  expect(@workspace.name.kind_of?String)
+end
 
+Then(/^I expect that the response attribute person_id be an integer$/) do
+  require_relative '../../../src/requests/Workspaces/workspaces_get'
+  expect(@workspace.person_id.kind_of?Integer)
+end
 
+Then(/^I expect that the response attribute project_ids equal to Array$/) do
+  require_relative '../../../src/requests/Workspaces/workspaces_get'
+  expect(@workspace.project_ids.kind_of?Array)
+end
 
+Then(/^I expect that the response attribute project_ids equal to nil$/) do
+  require_relative '../../../src/requests/Workspaces/workspaces_get'
+  expect(@workspace.project_ids.size()).to eql(0)
+end
 
+When(/^I expect that the response attribute project_ids equal to count (.*)$/) do |count|
+  require_relative '../../../src/requests/Workspaces/workspaces_get'
+  expect(@workspace.project_ids.size()).to eql(count.to_i)
+end
+
+Given(/^I send a POST request to WorkspacesPost endpoint with a name has (\d+) characters$/) do |quantity|
+  require_relative '../../../src/requests/Workspaces/workspaces_post'
+  @name_workspace = 'a' * quantity.to_i
+  json = "{ \"name\" : \"#{@name_workspace}\"}"
+  @http_response, @workspace = WorkspacesPost.call_and_return_response(@client, 'POST', json)
+end
+
+Given(/^I expect that response has the attribute name have (\d+) characters$/) do |quantity|
+  expect(@workspace.name.size()).to eql(quantity.to_i)
+end
--- src/requests/Workspaces/workspaces_put.rb
+++ src/requests/Workspaces/workspaces_put.rb
@@ -4,7 +4,8 @@ class WorkspacesPut
     http_request = client.get_request(method, end_point)
     http_request.body = json
     http_response = client.execute_request(client.get_connection, http_request)
-    http_response
+    workspaces = Workspaces.get_parser_workspaces(http_response.body)
+    return http_response, workspaces
   end
 end
 
