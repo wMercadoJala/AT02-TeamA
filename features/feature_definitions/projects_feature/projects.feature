14edc51c90ab71fb51112e49947fd715e5f4cb34
JorgeForeroM <Jorge.Forero@fundacion-jala.org>
2016-10-14 15:34:50 -0400

    Functional testing for projects

--- features/feature_definitions/projects_feature/projects.feature
+++ features/feature_definitions/projects_feature/projects.feature
@@ -1,29 +1,70 @@
 @api_test
 Feature: Projects Smoke Tests
 
-  @smoke @get
+  @smoke @project @get
   Scenario: Projects Get
     Given I have set a connection to pivotal_tracker API service
-    When I send a GET request to ProjectGet endpoint
-    Then I expect Status code 200 for the SmokeTest
+      And I have created a "Test" project
+    When I send a GET request to ProjectGet endpoint to "Test" project
+    Then I expect Status code 200 for the Project GET
 
-  @smoke @post
+  @smoke @project @post
   Scenario: Project Create
     Given I have set a connection to pivotal_tracker API service
     When I send a POST request to ProjectPost endpoint with the name "NewProject"
-    Then I expect Status code 200 for the Create SmokeTest
+    Then I expect Status code 200 for the Project Create
 
-  @smoke @put
+  @smoke @project @put
   Scenario: Project Edit
     Given I have set a connection to pivotal_tracker API service
       And I have created a "Test" project
     When I send a PUT request to ProjectPut endpoint change the name to "Modify"
-    Then I expect Status code 200 for the Edit SmokeTest
+    Then I expect Status code 200 for the Project Edit
 
 
-  @smoke @delete
+  @smoke @project @delete
   Scenario: Project Delete
     Given I have set a connection to pivotal_tracker API service
       And I have created a "Test" project
     When I send a DELETE request to ProjectDelete endpoint
-    Then I expect Status code 204 for the SmokeTest
+    Then I expect Status code 204 for the Project DELETE
+
+  @project @functional
+  Scenario: Project validation
+    Given I have set a connection to pivotal_tracker API service
+      And I have created a "Test" project
+    When I send a GET request to ProjectGet endpoint to "Test" project
+    Then I expect Status code 200 for the Project GET
+      And I expected if response has the attribute project name equal to "Test"
+      And I expected if response has the attribute project id is a integer
+      And I expected if response has the attribute project start date is DateTime
+      And I expected if response has the attribute project account id is a integer
+      And I expected if response has the attribute project iteration length is a integer
+      And I expected if response has the attribute project kind is equal to "project"
+
+  @project @post @functional
+  Scenario: Project Create
+    Given I have set a connection to pivotal_tracker API service
+    When I send a POST request to ProjectPost endpoint with name "NewProject", type "public"
+    Then I expect Status code 200 for the Project Create
+      And I expected if response has the attribute project type is equal to "public"
+
+  @project @post @functional
+  Scenario: Project Edit
+    Given I have set a connection to pivotal_tracker API service
+      And I have created a "Test" project
+    When I send a PUT request to ProjectPut endpoint changing the iteration_length to "2"
+    Then I expect Status code 200 for the Project Edit
+      And I expected if response has the attribute interaction length is equal to "2"
+
+  @project @post @negative
+  Scenario: Project Create
+    Given I have set a connection to pivotal_tracker API service
+    When I send a POST request to ProjectPost endpoint with name has 51 characters
+    Then I expect Status code 400 for the Project Create
+      And I expected if response has the message
+  """
+  'name' parameter must be 50 characters or less.
+  """
+
+
--- features/step_definitions/projects_steps/projects_steps.rb
+++ features/step_definitions/projects_steps/projects_steps.rb
@@ -1,25 +1,43 @@
 Given(/^I have created a "(.*)" project$/) do |name_project|
   require_relative '../../../src/requests/Projects/projects_post'
   json = "{ \"name\" : \"#{name_project}\"}"
-  @http_response_post, @project_name = ProjectsPost.call_and_return_response(@client, 'POST', json)
+  http_response_post, @project_name = ProjectsPost.call_and_return_response(@client, 'POST', json)
 end
 
-When(/^I send a (GET) request to ProjectGet endpoint$/) do |method|
+When(/^I send a (GET) request to ProjectGet endpoint to "Test" project$/) do |method|
   require_relative '../../../src/requests/Projects/projects_get'
-  @http_response = ProjectsGet.call_and_return_response(@client, method)
+  @http_response, @project_get = ProjectsGet.call_and_return_response(@client, method, @project_name.id)
 end
 
-When(/^I send a (POST) request to ProjectPost endpoint with the (.*) "(.*)"$/) do |method, attribute, name_project|
+When(/^I send a (POST) request to ProjectPost endpoint with the (.*) "(\w+)"$/) do |method, attribute, name_project|
   require_relative '../../../src/requests/Projects/projects_post'
   json = "{ \"#{attribute}\" : \"#{name_project}\"}"
   @http_response, @project_name = ProjectsPost.call_and_return_response(@client, method, json)
+end
 
+When(/^I send a (POST) request to ProjectPost endpoint with (.*) "(.*)", type "(.*)"$/) do |method, attribute, name_project, type|
+  require_relative '../../../src/requests/Projects/projects_post'
+  json = "{ \"#{attribute}\" : \"#{name_project}\", \"#{type}\" : true }"
+  @http_response, @project_name = ProjectsPost.call_and_return_response(@client, method, json)
+end
+
+When(/^I send a (POST) request to ProjectPost endpoint with (.*) has (\d+) characters$/) do |method, attribute, number|
+  require_relative '../../../src/requests/Projects/projects_post'
+  name_project = 'a' * number.to_i
+  json = "{ \"#{attribute}\" : \"#{name_project}\"}"
+  @http_response, @project_name = ProjectsPost.call_and_return_response(@client, method, json)
 end
 
 When(/^I send a (PUT) request to ProjectPut endpoint change the (.*) to "(.*)"$/) do |method, attribute, name_project|
   require_relative '../../../src/requests/Projects/projects_put'
   json = "{ \"#{attribute}\" : \"#{name_project}\"}"
-  @http_response = ProjectsPut.call_and_return_response(@client, method, json, @project_name.id)
+  @http_response, @project_name = ProjectsPut.call_and_return_response(@client, method, json, @project_name.id)
+end
+
+When(/^I send a (PUT) request to ProjectPut endpoint changing the (.*) to "(.*)"$/) do |method, attribute, iteration|
+  require_relative '../../../src/requests/Projects/projects_put'
+  json = "{ \"#{attribute}\" : #{iteration.to_i}"
+  @http_response, @project_name = ProjectsPut.call_and_return_response(@client, method, json, @project_name.id)
 end
 
 When(/^I send a (DELETE) request to ProjectDelete endpoint$/) do |method|
@@ -27,10 +45,52 @@ When(/^I send a (DELETE) request to ProjectDelete endpoint$/) do |method|
   @http_response = ProjectsDelete.call_and_return_response(@client, method, @project_name.id)
 end
 
-
-Then(/^I expect Status code (\d+) for the (.*) SmokeTest$/) do |http_code, method|
+Then(/^I expect Status code (\d+) for the Project (.*)$/) do |http_code, method|
   expect(@http_response.code.to_s).to eql(http_code.to_s)
   require_relative '../../../src/requests/Projects/projects_delete'
   ProjectsDelete.call_and_return_response(@client, 'DELETE', @project_name.id) unless method == 'DELETE'
 end
+
+Then(/^I expected if response has the attribute project name equal to "(.*)"$/) do |project_name|
+  expect(project_name.equal?@project_get.name)
+end
+
+Then(/^I expected if response has the attribute project id is a integer$/) do
+  expect(@project_get.id.kind_of?(Integer))
+end
+
+Then(/^I expected if response has the attribute project start date is DateTime$/) do
+  date_time = DateTime.parse(@project_get.start_time)
+  expect(date_time.kind_of?(DateTime))
+end
+
+Then(/^I expected if response has the attribute project account id is a integer$/) do
+  expect(@project_get.account_id.kind_of?(Integer))
+end
+
+Then(/^I expected if response has the attribute project iteration length is a integer$/) do
+  expect(@project_get.iteration_length.kind_of?(Integer))
+end
+
+Then(/^I expected if response has the attribute project kind is equal to "(.*)"$/) do |kind|
+  expect(kind.equal?@project_get.kind)
+end
+
+Then(/^I expected if response has the attribute project type is equal to "(.*)"$/) do |type|
+  expect(type.equal?@project_name.project_type)
+end
+
+Then(/^I expected if response has the attribute interaction length is equal to "(.*)"$/) do |interaction|
+  expect(interaction.to_i.equal?@project_name.iteration_length)
+end
+
+Then(/^I expected if response has the attribute name has (\d+) characters$/) do |number|
+  expect(number.to_i.equal?@project_name.name.size)
+end
+
+Then(/^I expected if response has the message$/) do |message|
+  expect(message.equal?@project_name.requirement)
+end
+
+
 
